b6daea56d6490acbf4eae4d7350ed0e9b9ee7c27
user638726 <lienjackson35@gmail.com>
2025-04-08 04:43:29 +0800

    建構網站

---
+++ resources/views/show.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+@section('title',$task->title)
+@section('content')
+<div class="mb-4">
+    <a href="{{ route('tasks.index') }} "
+        class="link"><-Go Back to the task list</a>
+</div>
+<p class="mb-4 text-slate-700">{{ $task->description }}</p>
+
+@if($task->long_description)
+  <p class="mb-4 text-slate-700">{{ $task->long_description  }}
+@endif
+
+ <p class="mb-4 text-sm text-slate-500">Create {{ $task->created_at->diffForHumans() }} ‧Updated {{ $task->updated_at->diffForHumans() }}</p>
+
+  <p class="mb-4">
+    @if($task->completed)
+    <span class="font-medium text-green-500">Completed</span>
+    @else
+    <span class="font-medium text-red-500">Not Completed</span>
+    @endif
+  </p>
+
+ <div class="flex gap-2">
+    <a href="{{ route('tasks.edit',['task'=>$task]) }}"
+        class="btn">Edit</a>
+    <form action="{{ route('tasks.toggle-complete',['task'=>$task]) }}" method="POST">
+       @csrf
+       @method('PUT')
+       <button type="submit" class="btn">
+         Mark as {{ $task->completed ? 'not completed':'completed' }}
+       </button>
+    </form>
+    <form action="{{ route('tasks.destroy', ['task'=>$task]) }}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button type="submit" class="btn">Delete</button>
+    </form>
+ </div>
+@endsection
